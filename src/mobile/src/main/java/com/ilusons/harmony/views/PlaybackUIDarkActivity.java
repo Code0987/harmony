c6e1052a819845b16abe58a42b1089e676e29797
Code0987 <durgapalneeraj@gmail.com>
2017-05-12 20:36:52 +0530

    updates

--- src/mobile/src/main/java/com/ilusons/harmony/views/PlaybackUIDarkActivity.java
+++ src/mobile/src/main/java/com/ilusons/harmony/views/PlaybackUIDarkActivity.java
@@ -1,37 +1,23 @@
 package com.ilusons.harmony.views;
 
 import android.content.ComponentName;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BlurMaskFilter;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.TransitionDrawable;
-import android.media.audiofx.Visualizer;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.graphics.ColorUtils;
 import android.support.v7.graphics.Palette;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 
 import com.ilusons.harmony.R;
@@ -39,11 +25,7 @@ import com.ilusons.harmony.base.BasePlaybackUIActivity;
 import com.ilusons.harmony.base.MusicService;
 import com.ilusons.harmony.data.Music;
 import com.ilusons.harmony.ref.JavaEx;
-import com.ilusons.harmony.ref.VisualizerEx;
 import com.wang.avi.AVLoadingIndicatorView;
-import com.yalantis.waves.util.Horizon;
-
-import java.util.ArrayList;
 
 public class PlaybackUIDarkActivity extends BasePlaybackUIActivity {
 
@@ -63,6 +45,7 @@ public class PlaybackUIDarkActivity extends BasePlaybackUIActivity {
     private FloatingActionButton fab_stop;
 
     private LyricsViewFragment lyricsViewFragment;
+    private AudioVFXViewFragment audioVFXViewFragment;
 
     private AVLoadingIndicatorView loadingView;
 
@@ -307,9 +290,15 @@ public class PlaybackUIDarkActivity extends BasePlaybackUIActivity {
                         fab_random.getBackground().setColorFilter(color, PorterDuff.Mode.SRC_IN);
                         fab_stop.getBackground().setColorFilter(color, PorterDuff.Mode.SRC_IN);
 
-                        // TODO: setupFXHorizon(color);
-                        // TODO: setupFXView(color);
-                        setupFXView(color);
+                        if (audioVFXViewFragment != null && audioVFXViewFragment.isAdded()) {
+                            audioVFXViewFragment.reset(getMusicService(), AudioVFXViewFragment.AVFXType.AVFX, color);
+                        } else {
+                            audioVFXViewFragment = AudioVFXViewFragment.create();
+                            getFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.avfx_layout, audioVFXViewFragment)
+                                    .commit();
+                        }
 
                         loadingView.hide();
 
@@ -371,348 +360,6 @@ public class PlaybackUIDarkActivity extends BasePlaybackUIActivity {
             }
         };
         handler.postDelayed(progressHandlerRunnable, dt);
-
-    }
-
-    private Horizon horizon;
-    private VisualizerEx horizonVisualizerEx;
-    private GLSurfaceView horizonView;
-
-    private void setupFXHorizon(int color) {
-        RelativeLayout parent = (RelativeLayout) findViewById(R.id.fx_layout);
-
-        if (horizonView != null)
-            parent.removeView(horizonView);
-
-        horizonView = new GLSurfaceView(this);
-
-        horizonView.setZOrderOnTop(true);
-        horizonView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
-        horizonView.getHolder().setFormat(PixelFormat.RGBA_8888);
-        // horizonView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-
-        parent.addView(horizonView, 0);
-
-        horizon = new Horizon(
-                horizonView,
-                Color.argb(255, 0, 0, 0),
-                44100,
-                1,
-                16);
-        horizon.setMaxVolumeDb(120);
-
-        try {
-            horizonVisualizerEx = new VisualizerEx(PlaybackUIDarkActivity.this, getMusicService().getAudioSessionId(), new VisualizerEx.OnFftDataCaptureListener() {
-                @Override
-                public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
-                    horizon.updateView(fft);
-                }
-            });
-            horizonVisualizerEx.setEnabled(true);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private FXView fxView;
-
-    private void setupFXView(int color) {
-        if (fxView != null) {
-            fxView.Color = color;
-            fxView.reset();
-
-            return;
-        }
-
-        RelativeLayout parent = (RelativeLayout) findViewById(R.id.fx_layout);
-
-        fxView = new FXView(this, parent.getWidth(), parent.getHeight(), color);
-
-        parent.addView(fxView, 0);
-
-        fxView.reset();
-    }
-
-    public class FXView extends SurfaceView implements SurfaceHolder.Callback {
-
-        private final String TAG = FXView.class.getName();
-
-        Context context;
-
-        public int Color;
-
-        private SurfaceHolder holder;
-        private UpdaterThread thread;
-
-        VisualizerEx visualizerEx;
-        private float[] dbs;
-        private float[] amps;
-        private int sr;
-
-        private int height;
-        private int width;
-
-        private ArrayList<Particle> particles = new ArrayList<>();
-
-        private Paint paint = new Paint();
-        private Path path = new Path();
-        private BlurMaskFilter blurFilter1 = new BlurMaskFilter(1, BlurMaskFilter.Blur.NORMAL);
-        private BlurMaskFilter blurFilter2 = new BlurMaskFilter(2, BlurMaskFilter.Blur.NORMAL);
-
-        public FXView(Context context, int width, int height, int color) {
-            super(context);
-
-            this.context = context;
-
-            this.Color = color;
-
-            holder = getHolder();
-            holder.addCallback(this);
-            holder.setFormat(PixelFormat.TRANSPARENT);
-
-            setZOrderOnTop(true);
-
-            this.width = width;
-            this.height = height;
-
-            setWillNotDraw(false);
-        }
-
-        @Override
-        public void surfaceCreated(SurfaceHolder holder) {
-        }
-
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            if (thread == null) {
-                thread = new UpdaterThread(holder);
-                thread.setRunning(true);
-                thread.start();
-            }
-
-            this.height = height;
-            this.width = width;
-        }
-
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            boolean retry = true;
-            thread.setRunning(false);
-            while (retry) {
-                try {
-                    thread.join();
-                    retry = false;
-                } catch (InterruptedException e) {
-                    Log.d(getClass().getSimpleName(), "Interrupted Exception", e);
-                }
-            }
-
-            if (visualizerEx != null) {
-                visualizerEx.release();
-            }
-        }
-
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            return super.onTouchEvent(event);
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-
-            Canvas c = canvas;
-
-            // c.drawColor(Color.TRANSPARENT, PorterDuff.Mode.OVERLAY);
-
-            for (int i = 0; i < particles.size(); i++) {
-
-                Particle p = particles.get(i);
-
-                p.draw(c);
-
-                for (int j = particles.size() - 1; j > i; j--) {
-                    float d = (float) Math.sqrt(
-                            Math.pow(particles.get(i).X - particles.get(j).X, 2)
-                                    +
-                                    Math.pow(particles.get(i).Y - particles.get(j).Y, 2)
-                    );
-
-                    if (d > p.Proximity)
-                        continue;
-
-                    paint.reset();
-                    paint.setAntiAlias(true);
-                    paint.setColor(Color);
-                    paint.setAlpha((int) (((p.Proximity - d) / p.Proximity) * 255));
-                    paint.setStyle(Paint.Style.FILL_AND_STROKE);
-                    paint.setStrokeWidth(1f);
-                    paint.setMaskFilter(blurFilter1);
-
-                    path.reset();
-                    path.moveTo(particles.get(i).X, particles.get(i).Y);
-                    path.lineTo(particles.get(j).X, particles.get(j).Y);
-                    path.close();
-
-                    c.drawPath(path, paint);
-                }
-            }
-
-        }
-
-        public void reset() {
-            try {
-                if (visualizerEx != null)
-                    visualizerEx.release();
-                visualizerEx = new VisualizerEx(PlaybackUIDarkActivity.this, getMusicService().getAudioSessionId(), new VisualizerEx.OnFftDataCaptureListener() {
-                    @Override
-                    public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
-                        int dataSize = fft.length / 2 - 1;
-
-                        sr = samplingRate;
-
-                        if (dbs == null || dbs.length != dataSize) {
-                            dbs = new float[dataSize];
-                        }
-                        if (amps == null || amps.length != dataSize) {
-                            amps = new float[dataSize];
-                        }
-
-                        for (int i = 0; i < dataSize; i++) {
-                            float re = fft[2 * i];
-                            float im = fft[2 * i + 1];
-                            float sq = re * re + im * im;
-
-                            dbs[i] = (float) (sq > 0 ? 20 * Math.log10(sq) : 0);
-
-                            float k = 1;
-                            if (i == 0 || i == dataSize - 1) {
-                                k = 2;
-                            }
-
-                            amps[i] = (float) (k * Math.sqrt(sq) / dataSize);
-                        }
-                    }
-                });
-                visualizerEx.setEnabled(true);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        public class UpdaterThread extends Thread {
-            private final SurfaceHolder holder;
-
-            private boolean running = false;
-
-            public UpdaterThread(SurfaceHolder holder) {
-                this.holder = holder;
-            }
-
-            @Override
-            public void run() {
-                Canvas canvas = null;
-
-                while (running) {
-
-                    if (particles.size() < 25) {
-                        while (particles.size() < 25)
-                            particles.add(new Particle());
-                    }
-
-                    for (int i = 0; i < particles.size(); i++) {
-
-                        Particle p = particles.get(i);
-
-                        p.update();
-                    }
-
-                    try {
-                        canvas = holder.lockCanvas(null);
-                        synchronized (holder) {
-                            postInvalidate();
-                        }
-                    } finally {
-                        if (canvas != null) {
-                            holder.unlockCanvasAndPost(canvas);
-                        }
-                    }
-                }
-
-            }
-
-            public void setRunning(boolean b) {
-                running = b;
-            }
-        }
-
-        class Particle {
-            public float R = 1.0f;
-            public float X;
-            public float Y;
-            public float Vx;
-            public float Vy;
-            public float Proximity;
-
-            public Particle() {
-                X = (float) Math.random() * width;
-                Y = (float) Math.random() * height;
-                Vx = ((float) Math.random() - 0.5f) * 5;
-                Vy = ((float) Math.random() - 0.5f) * 5;
-                Proximity = ((float) Math.random() * 0.24f * (float) Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2)));
-            }
-
-            public void update() {
-                if (X > width + 20 || X < -20) {
-                    Vx = -Vx;
-                }
-                if (Y > height + 20 || Y < -20) {
-                    Vy = -Vy;
-                }
-
-                float db = 0;
-                float amp = 0;
-                int k = 0;
-
-                float y = 0;
-
-                if (dbs != null && amps != null) {
-                    k = (int) ((X / width) * dbs.length);
-                    k = Math.abs(k);
-                    k = Math.min(dbs.length - 1, Math.max(0, k));
-
-                    db = dbs[k];
-                    amp = amps[k];
-
-                    y = amp * -k * 100;
-
-                    // Log.d(TAG, "fft = " + db + "db, " + amp + "amp" + ". k=" + k + ", y=" + y);
-                }
-
-                if (Math.abs(y) < 10) {
-                    // X += Vx;
-                    Y += Vy;
-                } else {
-                    // X += Vx;
-                    Y += (-Y + (height / 2 - R + y + 50));
-                }
-
-            }
-
-            public void draw(Canvas c) {
-                Paint paint = new Paint();
-                paint.setAntiAlias(true);
-                paint.setColor(Color);
-                paint.setStyle(Paint.Style.FILL_AND_STROKE);
-                paint.setStrokeWidth(3f);
-                paint.setMaskFilter(blurFilter2);
-
-                c.drawCircle(X + R, Y + R, R, paint);
-            }
-        }
 
     }
 
