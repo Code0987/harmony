1e00a912c68eff9b16cc08b1ca914acb57d51a13
Code0987 <durgapalneeraj@gmail.com>
2017-05-17 01:09:37 +0530

    updates

--- src/mobile/src/main/java/com/ilusons/harmony/ref/AndroidEx.java
+++ src/mobile/src/main/java/com/ilusons/harmony/ref/AndroidEx.java
@@ -1,4 +1,22 @@
 package com.ilusons.harmony.ref;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.content.IntentCompat;
+
+
 public class AndroidEx {
+
+    public static final String TAG = AndroidEx.class.getSimpleName();
+
+    public static void restartApp(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        Intent intent = packageManager.getLaunchIntentForPackage(context.getPackageName());
+        ComponentName componentName = intent.getComponent();
+        Intent mainIntent = IntentCompat.makeRestartActivityTask(componentName);
+        context.startActivity(mainIntent);
+        System.exit(0);
+    }
 }
