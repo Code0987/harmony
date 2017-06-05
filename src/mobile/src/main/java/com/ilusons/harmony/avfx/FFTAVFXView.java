a753cf70befce34b8b5376cd5a5be4c6ae12a21e
Code0987 <durgapalneeraj@gmail.com>
2017-06-05 22:48:43 +0530

    updates

--- src/mobile/src/main/java/com/ilusons/harmony/avfx/FFTAVFXView.java
+++ src/mobile/src/main/java/com/ilusons/harmony/avfx/FFTAVFXView.java
@@ -40,48 +40,86 @@ public class FFTAVFXView extends BaseAVFXView {
 
     @Override
     protected void onRenderAudioData(GL10 gl, int width, int height, Buffer data) {
-        final int canvasWidth = width;
-        final int canvasHeight = height;
-        final float[] fft = data.fData;
+        if (data.fData != null) {
+            final int canvasWidth = width;
+            final int canvasHeight = height;
+            final float[] fft = data.fData;
 
-        // -2, +2: (DC + Fs/2), /2: (Re + Im)
-        final int N = ((fft.length / 2) - 2) / 2 + 2;
+            // -2, +2: (DC + Fs/2), /2: (Re + Im)
+            final int N = ((fft.length / 2) - 2) / 2 + 2;
 
-        // 2: (x, y)
-        final int workBufferSize = N * 2;
-        boolean needToUpdateX = false;
+            // 2: (x, y)
+            final int workBufferSize = N * 2;
+            boolean needToUpdateX = false;
 
-        // prepare working buffer
-        if (buffer == null || buffer.length < workBufferSize) {
-            buffer = new float[workBufferSize];
-            nativeBuffer = allocateNativeFloatBuffer(workBufferSize);
-            needToUpdateX |= true;
+            // prepare working buffer
+            if (buffer == null || buffer.length < workBufferSize) {
+                buffer = new float[workBufferSize];
+                nativeBuffer = allocateNativeFloatBuffer(workBufferSize);
+                needToUpdateX |= true;
+            }
+
+            final float[] points = buffer;
+            final FloatBuffer pointsBuffer = nativeBuffer;
+
+            // prepare points info buffer for drawing
+
+            needToUpdateX |= (width != lastCanvasWidth);
+            lastCanvasWidth = width;
+
+            if (needToUpdateX) {
+                makeXPointPositionData(N, points);
+            }
+
+            final int data_range = (N - 1) / 2;
+            final float yrange = data_range * 1.05f;
+
+            // Lch
+            makeYPointPositionData(fft, N, 0, points);
+            converToFloatBuffer(pointsBuffer, points, (2 * N));
+            drawFFT(gl, canvasWidth, canvasHeight, pointsBuffer, N, 0, yrange, leftChannelColor);
+
+            // Rch
+            makeYPointPositionData(fft, N, (N - 1) * 2, points);
+            converToFloatBuffer(pointsBuffer, points, (2 * N));
+            drawFFT(gl, canvasWidth, canvasHeight, pointsBuffer, N, 1, yrange, rightChannelColor);
         }
 
-        final float[] points = buffer;
-        final FloatBuffer pointsBuffer = nativeBuffer;
+        if (data.bData != null) {
+            final byte[] fft = data.bData;
 
-        // prepare points info buffer for drawing
+            // -2, +2: (DC + Fs/2), /2: (Re + Im)
+            final int N = (fft.length - 2) / 2 + 2;
 
-        needToUpdateX |= (width != lastCanvasWidth);
-        lastCanvasWidth = width;
+            // 2: (x, y)
+            final int workBufferSize = N * 2;
+            boolean needToUpdateX = false;
 
-        if (needToUpdateX) {
-            makeXPointPositionData(N, points);
+            // prepare working buffer
+            if (buffer == null || buffer.length < workBufferSize) {
+                buffer = new float[workBufferSize];
+                nativeBuffer = allocateNativeFloatBuffer(workBufferSize);
+                needToUpdateX |= true;
+            }
+
+            final float[] points = buffer;
+            final FloatBuffer pointsBuffer = nativeBuffer;
+
+            // prepare points info buffer for drawing
+
+            needToUpdateX |= (width != lastCanvasWidth);
+            lastCanvasWidth = width;
+
+            if (needToUpdateX) {
+                makeXPointPositionData(N, points);
+            }
+
+            final float yrange = 128.0f + 1.0f;
+
+            makeYPointPositionData(fft, N, 0, points);
+            converToFloatBuffer(pointsBuffer, points, (2 * N));
+            drawFFT(gl, width, height, pointsBuffer, N, yrange, leftChannelColor);
         }
-
-        final int data_range = (N - 1) / 2;
-        final float yrange = data_range * 1.05f;
-
-        // Lch
-        makeYPointPositionData(fft, N, 0, points);
-        converToFloatBuffer(pointsBuffer, points, (2 * N));
-        drawFFT(gl, canvasWidth, canvasHeight, pointsBuffer, N, 0, yrange, leftChannelColor);
-
-        // Rch
-        makeYPointPositionData(fft, N, (N - 1) * 2, points);
-        converToFloatBuffer(pointsBuffer, points, (2 * N));
-        drawFFT(gl, canvasWidth, canvasHeight, pointsBuffer, N, 1, yrange, rightChannelColor);
     }
 
     private void makeXPointPositionData(int N, float[] points) {
@@ -116,6 +154,49 @@ public class FFTAVFXView extends BaseAVFXView {
         gl.glViewport(0, (height / 2) * (1 - vposition), width, (height / 2));
 
         // X: [0:1], Y: [0:1]
+        gl.glOrthof(0.0f, 1.0f, 0.0f, yrange, -1.0f, 1.0f);
+
+        gl.glColor4f(color.red, color.green, color.blue, color.alpha);
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glVertexPointer(2, GL10.GL_FLOAT, (2 * Float.SIZE / 8), vertices);
+        gl.glDrawArrays(GL10.GL_LINE_STRIP, 0, n);
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+
+        gl.glPopMatrix();
+    }
+
+    private static void makeYPointPositionData(byte[] fft, int n, int offset, float[] points) {
+        // NOTE:
+        // mag = sqrt( (re / 128)^2 + (im / 128)^2 )
+        // = sqrt( (1 / 128)^2 * (re^2 + im^2) )
+        // = (1 / 128) * sqrt(re^2 + im^2)
+        //
+        // data range = [0:128] (NOTE: L + R mixed gain)
+
+        // DC
+        points[2 * 0 + 1] = Math.abs(fft[offset + 0]);
+
+        // f_1 .. f_(N-1)
+        for (int i = 1; i < (n - 1); i++) {
+            final int re = fft[offset + 2 * i + 0];
+            final int im = fft[offset + 2 * i + 1];
+            final float y = fastSqrt((re * re) + (im * im));
+
+            points[2 * i + 1] = y;
+        }
+
+        // fs / 2
+        points[2 * (n - 1) + 1] = Math.abs(fft[offset + 1]);
+    }
+
+    private static void drawFFT(GL10 gl, int width, int height, FloatBuffer vertices, int n, float yrange, FloatColor color) {
+
+        gl.glPushMatrix();
+
+        // viewport
+        gl.glViewport(0, 0, width, height);
+
+        // X: [0:1], Y: [0:yrange]
         gl.glOrthof(0.0f, 1.0f, 0.0f, yrange, -1.0f, 1.0f);
 
         gl.glColor4f(color.red, color.green, color.blue, color.alpha);
