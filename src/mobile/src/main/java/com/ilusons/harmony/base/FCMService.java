8663875fabe075303303f9218ee0b10dc71b51e0
Code0987 <durgapalneeraj@gmail.com>
2017-07-08 03:17:59 +0530

    fix: ads updates: fcm

---
+++ src/mobile/src/main/java/com/ilusons/harmony/base/FCMService.java
@@ -0,0 +1,78 @@
+package com.ilusons.harmony.base;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.ilusons.harmony.R;
+
+public class FCMService extends FirebaseMessagingService {
+
+    private static final String TAG = FCMService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "onMessageReceived\n" + remoteMessage);
+
+        try {
+            String title = null;
+            String content = null;
+            String link = null;
+
+            // Check if message contains a notification payload.
+            if (remoteMessage.getNotification() != null) {
+                title = remoteMessage.getNotification().getTitle();
+                content = remoteMessage.getNotification().getBody();
+            }
+
+            // Check if message contains a data payload.
+            if (remoteMessage.getData().size() > 0) {
+                if (TextUtils.isEmpty(title))
+                    title = remoteMessage.getData().get("title");
+                if (TextUtils.isEmpty(content))
+                    content = remoteMessage.getData().get("content");
+                link = remoteMessage.getData().get("link");
+            }
+
+            if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(content) && !title.trim().toLowerCase().equals("test"))
+                sendNotification(title, content, link);
+
+        } catch (Exception e) {
+            Log.w(TAG, e);
+        }
+
+    }
+
+    private void sendNotification(String title, String content, String link) {
+        PendingIntent pendingIntent = null;
+        if (TextUtils.isEmpty(link)) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(link));
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+
+        if (pendingIntent != null)
+            notificationBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+}
